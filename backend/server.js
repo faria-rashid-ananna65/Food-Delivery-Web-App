4bfe147c1d836ad0cfa7bcfc6f621b2da83a4766
Faria Rashid Ananna <fariarashidananna@gmail.com>
2026-08-28 13:50:17 +0600

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,11 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
