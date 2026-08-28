dd613f3bb8c78b92076a53ba18482cf2914b2956
Faria Rashid Ananna <fariarashidananna@gmail.com>
2026-08-28 16:43:11 +0600

    fix: move export to top level, export outside if block

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ import app from "./src/app.js";
 
 connectDB();
 
-if (process.env.VERCEL) {
-  export default app;
-} else {
+if (!process.env.VERCEL) {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
+
+export default app;
